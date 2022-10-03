98a4e8df8549d91d441b13f548cd574c4fcaa5a1
Tony Fu <tonyfu@fastmail.com>
2022-10-03 22:47:44 +1100

    Update omniboard

--- src/Omniboard.jsx
+++ src/Omniboard.jsx
@@ -1,20 +1,34 @@
 import React from "react";
 import Button from "./Button";
-import {setDocumentImportsAtom, documentImportsAtom} from "./typeings"
+import { setDocumentImportsAtom, documentImportsAtom } from "./typeings"
+import { currentDocPathAtom } from "./filesystem";
 import { useAtom } from "jotai";
 
 export default () => {
     let [, setImports] = useAtom(setDocumentImportsAtom);
-    let [documentImports,] = useAtom(documentImportsAtom)
-    return (<div>
-        <Button onClick={()=> {
+    let [docPath] = useAtom(currentDocPathAtom);
+    let [documentImports,] = useAtom(documentImportsAtom);
+    return (<div className="px-2 py-4">
+        <Button onClick={() => {
             setImports()
 
         }}>Get imports</Button>
         <div>
-        {documentImports.map((im, i) => {
-            return (<div key={i}>{im.signature} </div>)
-        })}
+            <div>Improted Functions</div>
+            <div className="bg-green-100 py-1 px-1">
+                {documentImports
+                    .filter((im) => im.modulePath !== docPath)
+                    .map((im, i) => {
+                        return (<div className="py-1" key={i}>
+                            <div className="font-mono">
+                                {im.signature}
+                            </div>
+                            <div className="text-sm">
+                                from {im.modulePath}
+                            </div>
+                        </div>)
+                    })}
+            </div>
 
         </div>
 
